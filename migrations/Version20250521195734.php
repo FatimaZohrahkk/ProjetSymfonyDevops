2e09e246948f5c4cc6806cc88edcfdbe036d6edd
fatimazahraTagmouti <fatitagmouti03@gmail.com>
2025-05-21 21:02:13 +0100

    fixing

--- migrations/Version20250521195734.php
+++ migrations/Version20250521195734.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250521191030 extends AbstractMigration
+final class Version20250521195734 extends AbstractMigration
 {
     public function getDescription(): string
     {
